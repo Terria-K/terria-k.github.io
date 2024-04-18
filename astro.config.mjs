bcb5b42ee34fbce0e84e5e6243b759316b67899c
Terria-K <aelbrian028@gmail.com>
2024-04-18 21:43:35 +0800

    Fixed base and site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://terria-k.github.io",
+    base: "teurian",
     integrations: [tailwind(), icon()]
 });
