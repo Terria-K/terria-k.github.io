682e864d4d5e84a05cc14dcb5d3fe2b28520ed68
Terria-K <aelbrian028@gmail.com>
2024-04-21 22:00:43 +0800

    use sharp image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon'
 import mdx from '@astrojs/mdx';
+import sharp from 'sharp';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://terria-k.github.io",
     integrations: [tailwind(), icon(), mdx()],
     image: {
-        service: passthroughImageService()
+        service: sharpImageService()
     }
 });
