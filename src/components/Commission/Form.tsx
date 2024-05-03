4d8ab7aa5dbf3d145fbe95d1ef99802228d7bd87
Terria-K <aelbrian028@gmail.com>
2024-05-03 14:43:34 +0800

    Revert "use Qwik instead of SolidJS"
    This reverts commit fc0ca94495a08f10e1bdf86edf513e85abca83a8.

--- src/components/Commission/Form.tsx
+++ src/components/Commission/Form.tsx
@@ -1,4 +1,4 @@
-import { $, Slot, component$, useSignal, useStore, sync$ } from "@builder.io/qwik"
+import { createSignal, type Component, type JSX } from "solid-js";
 
 type FormField = {
   platform: string,
@@ -13,12 +13,12 @@ type FormProp = {
 }
 
 
-const Form = component$((prop: FormProp) => {
-  const platform = useSignal("Email");
-  const response = useSignal<string|undefined>(undefined);
-  const responseColor = useSignal("text-red-500");
-  let form = useStore<FormField>({
-    platform: platform.value,
+const Form: Component<FormProp> = (prop) => {
+  const [platform, setPlatform] = createSignal("Email");
+  const [response, setResponse] = createSignal<string|undefined>(undefined);
+  const [responseColor, setResponseColor] = createSignal("text-red-500");
+  const [form, setForm] = createSignal<FormField>({
+    platform: platform(),
     contactname: "",
     title: "",
     description: "",
@@ -27,19 +27,27 @@ const Form = component$((prop: FormProp) => {
 
 
   function updateFormField(formName: string) {
-    return $((e: Event) => {
+    return (e: Event) => {
+      const inputElement = e.currentTarget as HTMLInputElement;
       //@ts-ignore
-      form[formName] = e.target.value!;
-    })
+      setForm({
+        ...form(),
+        [formName]: inputElement.value
+      });
+    }
   }
 
-  const onPlatformChange$ = $(function onPlatformChange(event: Event & { currentTarget: HTMLSelectElement; target: HTMLSelectElement; }) {
-    platform.value = event.target.value;
-    form.platform = event.target.value;
-  })
+  function onPlatformChange(event: Event & { currentTarget: HTMLSelectElement; target: HTMLSelectElement; }) {
+    setPlatform(event.target.value);
+    setForm({
+      ...form(),
+      platform: event.target.value
+    });
+  }
 
-  const onSubmit$ = $(async function onSubmit(e: Event) {
-    const formParam = form;
+  async function onSubmit(e: Event) {
+    e.preventDefault();
+    const formParam = form();
 
     const platform = formParam.platform;
 
@@ -49,20 +57,20 @@ const Form = component$((prop: FormProp) => {
     let reference = formParam.reference;
 
     if (contactName === "") {
-      response.value = "Please provide your contact name.";
-      responseColor.value = "text-red-500";
+      setResponse("Please provide your contact name.");
+      setResponseColor("text-red-500");
       return;
     }
 
     if (title === "") {
-      response.value = "Please provide the title of the art.";
-      responseColor.value = "text-red-500";
+      setResponse("Please provide the title of the art.");
+      setResponseColor("text-red-500");
       return;
     }
 
     if (description === "") {
-      response.value = "Please provide the details of the art.";
-      responseColor.value = "text-red-500";
+      setResponse("Please provide the details of the art.");
+      setResponseColor("text-red-500");
       return;
     }
     
@@ -106,43 +114,43 @@ const Form = component$((prop: FormProp) => {
     });
 
     if (res.ok) {
-      response.value = "Your order has been sent!";
-      responseColor.value = "text-green-500";
+      setResponseColor("text-green-500")
+      setResponse("Your order has been sent!");
     } else {
-      response.value = "Something went wrong with sending your order. Please try again later.";
-      responseColor.value = "text-green-500";
+      setResponseColor("text-red-500")
+      setResponse("Something went wrong with sending your order. Please try again later.");
     }
-  })
+  }
 
 
   return (
-  <form class="grid gap-4" onSubmit$={[sync$((event: Event) => event.preventDefault()), onSubmit$]}>
+  <form class="grid gap-4" onSubmit={onSubmit}>
     <p class="text-white font-bold text-2xl">Contact</p>
     <Label>
       Platform: *
       <select name="platform" class="py-2 px-2 bg-transparent rounded border-2 border-midnight-light" 
-        onChange$={onPlatformChange$} value={platform.value}>
+        onChange={onPlatformChange} value={platform()}>
         <option class="bg-midnight-light" value="Email">Email</option>
         <option class="bg-midnight-light" value="Discord">Discord</option>
       </select>
     </Label>
     <Label>
-      {platform.value}: *
-      <Input name="contactname" type="text" onChange={updateFormField("contactname")} value={form.contactname} />
+      {platform()}: *
+      <Input name="contactname" type="text" onChange={updateFormField("contactname")} value={form().contactname} />
     </Label>
     <p class="text-white font-bold text-2xl">Request</p>
     <Label>
       Art Title: *
-      <Input name="title" type="text" onChange={updateFormField("title")} value={form.title}/>
+      <Input name="title" type="text" onChange={updateFormField("title")} value={form().title}/>
     </Label>
     <Label>
       Details: *
-      <textarea onChange$={updateFormField("description")} class="py-2 px-2 bg-transparent rounded border-2 border-midnight-light" name="description" rows={5} cols={30} value={form.description}></textarea>
+      <textarea onChange={updateFormField("description")} class="py-2 px-2 bg-transparent rounded border-2 border-midnight-light" name="description" rows="5" cols="30" value={form().description}></textarea>
     </Label>
     <Label>
       References or Idea:
       <p class="text-gray-500 text-sm">You can put a links for references or idea to have a clear idea of what you want.</p>
-      <textarea onChange$={updateFormField("reference")} class="py-2 px-2 bg-transparent rounded border-2 border-midnight-light" name="reference" rows={2} cols={30} value={form.reference}></textarea>
+      <textarea onChange={updateFormField("reference")} class="py-2 px-2 bg-transparent rounded border-2 border-midnight-light" name="reference" rows="2" cols="30" value={form().reference}></textarea>
     </Label>
     <p class="text-gray-300 text-lg">
       Prices of each art is minimum of $5 and you can go beyond that if you want to support me more as an artist. <br/>
@@ -152,24 +160,22 @@ const Form = component$((prop: FormProp) => {
       <input class="hover:bg-green-400 bg-green-600 duration-300 text-white px-8 py-4 text-xl rounded-xl cursor-pointer" type="submit" value="Order"/>
     </div>
     {
-      response.value ? <p class={`${responseColor.value}`}>{response.value}</p> : null
+      response() ? <p class={`${responseColor()}`}>{response()}</p> : null
     }
   </form>)
-});
+}
 
-const Label = component$(() => {
-  return (<label class="flex flex-col text-white">
-    <Slot/>
-  </label>)
-});
+const Label: Component<{children?: JSX.Element }> = (prop) => {
+  return <label class="flex flex-col text-white">{prop.children}</label>
+}
 
-const Input = component$((prop: {
+const Input: Component<{
   type: string,
   name?: string,
   value?: string,
   onChange: (e: Event) => void
-}) => {
-  return <input name={prop.name} onChange$={prop.onChange} class="py-2 px-2 bg-transparent rounded border-2 border-midnight-light" type={prop.type} value={prop.value}/>
-});
+}> = (prop) => {
+  return <input name={prop.name} onChange={prop.onChange} class="py-2 px-2 bg-transparent rounded border-2 border-midnight-light" type={prop.type} value={prop.value}/>
+}
 
 export default Form;
