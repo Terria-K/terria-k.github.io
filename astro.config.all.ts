1dfed51831dff1d44bd0f000909253c9d6cb8f17
Terria-K <aelbrian028@gmail.com>
2024-05-13 14:30:27 +0800

    Footer changes and playform inline

--- astro.config.all.ts
+++ astro.config.all.ts
@@ -3,9 +3,12 @@ import icon from 'astro-icon'
 import mdx from '@astrojs/mdx';
 import metaTags from "astro-meta-tags";
 import solid from "@astrojs/solid-js";
+import playformInline from "@playform/inline";
 
 export function teuriaIntegrations() {
     return [
-        tailwind(), icon(), mdx(), metaTags(), solid()
+        tailwind(), icon(), mdx(), metaTags(), solid(), playformInline({
+            Critters: true
+        })
     ]
 }
