e3a10dfa8f11668e57b69582bbca1cb457d3da85
Terria-K <aelbrian028@gmail.com>
2024-06-09 16:23:49 +0800

    pushing to prod

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,7 @@
-import { db, Comment, User } from 'astro:db';
-import { v4 } from 'uuid';
+import { db, User } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	const terriaID = v4();
 	await db.insert(User).values([
 		{ 
 			_id: "-1", 
@@ -11,18 +9,6 @@ export default async function seed() {
 			email: "guest@email.com", 
 			password: "securedpasswordever",
 			isVerified: true,
-		},
-		{ 
-			_id: terriaID, 
-			username: "Terria", 
-			email: "terria@email.com", 
-			password: "securedpasswordever",
-			isVerified: true,
 		}
-	])
-
-	await db.insert(Comment).values([
-		{  author: terriaID, artRef: "Relentless Night", date: new Date(), body: "Coolio!" },
-		{  author: "-1", artRef: "Relentless Night", date: new Date(), body: "Wooh!" },
 	])
 }
