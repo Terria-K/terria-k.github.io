b82f885631947163a7cbb05df831a0bfe783bc60
Terria-K <aelbrian028@gmail.com>
2024-06-16 22:50:12 +0800

    No highlight for navbutton

--- src/pages/api/authVerify.ts
+++ src/pages/api/authVerify.ts
@@ -3,18 +3,17 @@ import { jwtVerify, verifyToken } from "../../lib/auth";
 
 export const prerender = false;
 
-
 export const GET: APIRoute = async (ctx) => {
-    const tokenVerified = await verifyToken(ctx.request);
+  const tokenVerified = await verifyToken(ctx.request);
 
-    if (!tokenVerified) {
-        return new Response("false");
-    }
+  if (!tokenVerified) {
+    return new Response("false");
+  }
 
-    try {
-        const user = await jwtVerify(tokenVerified as string, import.meta.env.JWT_SECRET_KEY);
-        return new Response("true");
-    } catch (err) {
-        return new Response("false");
-    }
-}
+  try {
+    await jwtVerify(tokenVerified as string, import.meta.env.JWT_SECRET_KEY);
+    return new Response("true");
+  } catch (err) {
+    return new Response("false");
+  }
+};
--- src/pages/api/resend.ts
+++ src/pages/api/resend.ts
@@ -1,33 +1,36 @@
 import type { APIRoute } from "astro";
-import { sendMail } from "../../lib/mail";
 import { User, db, eq, or } from "astro:db";
+import { sendMail } from "../../lib/mail";
 
 export const prerender = false;
 
 export const GET: APIRoute = async (ctx) => {
-    const name = ctx.url.searchParams.get("receiver")?.valueOf();
-    if (!name) {
-        return new Response("Something went wrong.", { status: 401 });
-    }
+  const name = ctx.url.searchParams.get("receiver")?.valueOf();
+  if (!name) {
+    return new Response("Something went wrong.", { status: 401 });
+  }
 
-    let findIfExists = await db.select().from(User).where(
-        or(
-            eq(User.email, name), 
-            eq(User.username, name))
-        );
+  let findIfExists = await db
+    .select()
+    .from(User)
+    .where(or(eq(User.email, name), eq(User.username, name)));
 
-    let user = findIfExists.at(0);
+  let user = findIfExists.at(0);
 
-    if (!user) {
-        return new Response("Account does not existed with this email or username.", { status: 401 });
-    }
+  if (!user) {
+    return new Response(
+      "Account does not existed with this email or username.",
+      { status: 401 },
+    );
+  }
 
-    if (user.emailToken) {
-        sendMail(ctx.url.href, user.email, user.emailToken);
-    } else {
-        return new Response("This user has already been verified.");
-    }
+  if (user.emailToken) {
+    sendMail(ctx.url.href, user.email, user.emailToken);
+  } else {
+    return new Response("This user has already been verified.");
+  }
 
-
-    return new Response("The verification token has been resend to your email. Please check your inbox or spam.");
-}
+  return new Response(
+    "The verification token has been resend to your email. Please check your inbox or spam.",
+  );
+};
