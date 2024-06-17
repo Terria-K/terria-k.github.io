7931c8d86a653d964c9b1198b3742219594ca19a
Terria-K <aelbrian028@gmail.com>
2024-06-17 22:37:35 +0800

    site-canvas

---
+++ src/components/@svelte-components/site-canvas.svelte
@@ -0,0 +1,22 @@
+<svelte:options customElement="site-canvas"/>
+<script lang="ts">
+  import html from "rasterizehtml";
+  import { onMount } from "svelte";
+  let canvas: HTMLCanvasElement;
+
+  export let url: string;
+
+  onMount(() => {
+    html.drawURL(url, canvas);
+  })
+</script>
+
+<canvas width="1245" height="786" bind:this={canvas}></canvas>
+
+<style>
+  canvas {
+    object-fit: cover;
+    width: 622.5px;
+    height: 393px;
+  }
+</style>
