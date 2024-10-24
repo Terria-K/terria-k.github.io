20da1c0987226930a768fca2780d029bf82e5f8d
Terria-K <teuria@proton.me>
2024-10-24 12:50:16 +0800

    Optimizes image output

--- astro.config.all.ts
+++ astro.config.all.ts
@@ -4,11 +4,13 @@ import mdx from '@astrojs/mdx';
 import metaTags from "astro-meta-tags";
 import playformInline from "@playform/inline";
 import svelte from "@astrojs/svelte";
+import removeOriginalImages from './src/hooks/removeOriginalImages';
 
 export function teuriaIntegrations() {
     return [
         tailwind(), icon(), mdx(), metaTags(), svelte(), playformInline({
             Critters: true
-        })
+        }),
+        removeOriginalImages()
     ]
 }
---
+++ src/hooks/removeOriginalImages.ts
@@ -0,0 +1,66 @@
+// https://github.com/withastro/astro/issues/4961#issuecomment-2416411347
+import { readdir, unlink } from "node:fs/promises";
+import path from "node:path";
+import type { AstroIntegration, AstroConfig } from "astro";
+
+function removeOriginalImages() {
+  let astroConfig: AstroConfig;
+
+  // Used to identify original image files
+  const ORIGINAL_IMAGE_FORMATS = ["jpg", "jpeg", "png", "webp"] as const;
+  // Regex pattern: `dot eight hash chars dot`
+  const ORIGINAL_IMAGE_HASH_PATTERN = "\\.[a-zA-Z0-9_\\-]{8}\\.";
+
+  const integration: AstroIntegration = {
+    name: "ROI",
+    hooks: {
+      "astro:config:done": ({ config }) => {
+        astroConfig = config;
+      },
+      "astro:build:done": async ({ dir, logger }) => {
+        const astroAssetsDir = path.join(dir.pathname, astroConfig.build.assets);
+        const files = await readdir(astroAssetsDir);
+
+        let foundFilesCount = 0;
+        let removedFilesCount = 0;
+
+        for (const file of files) {
+          const { ext } = path.parse(file);
+          // Strip `ext` of dot also for use in upcoming regex match, as dots have special meaning
+          const fileFormat = ext.slice(1);
+
+          if (!(ORIGINAL_IMAGE_FORMATS as ReadonlyArray<string>).includes(fileFormat)) continue;
+
+          // Match original image files by ending with single hash and extension
+          const reOriginalImage = new RegExp(`${ORIGINAL_IMAGE_HASH_PATTERN}${fileFormat}$`);
+          if (!reOriginalImage.test(file)) continue;
+
+          foundFilesCount++;
+          logger.warn(`Removing ${file}`);
+
+          const result = await unlink(path.join(astroAssetsDir, file));
+
+          if (isUnlinkSuccessful(result)) {
+            removedFilesCount++;
+          } else {
+            logger.error(`Couldn't remove file ${file}`);
+          }
+        }
+
+        if (foundFilesCount > 0) {
+          logger.warn(`Removed ${removedFilesCount}/${foundFilesCount} files.`);
+        }
+
+        /** Helper function */
+        function isUnlinkSuccessful(result: unknown): boolean {
+          // https://nodejs.org/docs/latest-v20.x/api/fs.html#fspromisesunlinkpath
+          return typeof result === "undefined";
+        }
+      },
+    },
+  };
+
+  return integration;
+}
+
+export default removeOriginalImages;
