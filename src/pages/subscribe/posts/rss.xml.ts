0eb2f7e87e765b6bc62b1698ffd5bc60c7bbb944
Terria-K <teuria@proton.me>
2025-02-14 14:27:49 +0800

    Fixed RSS URL

--- src/pages/subscribe/posts/rss.xml.ts
+++ src/pages/subscribe/posts/rss.xml.ts
@@ -13,7 +13,7 @@ export const GET: APIRoute = async ({ }) => {
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      link: `/posts/${post.id}/`,
+      link: `/posts/${post.id}/`.replaceAll(".mdx", ""),
     })),
   });
 }
